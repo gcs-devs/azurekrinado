bae0d725aad23bf120095bc6c8fc92fe96546477
Guilherme Costa <53190988+gcs-devs@users.noreply.github.com>
2025-02-03 16:41:01 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,33 +1,17 @@
 variable "diskAccesses_setup_name" {
-
   description = "Nome do disk access setup"
-
   type        = string
-
   default     = "setup"
-
 }
-
- 
 
 variable "accounts_teste_name" {
-
   description = "Nome da conta de teste"
-
   type        = string
-
   default     = "teste"
-
 }
 
- 
-
 variable "storageAccounts_procon_name" {
-
   description = "Nome da conta de armazenamento procon"
-
   type        = string
-
   default     = "procon"
-
 }
