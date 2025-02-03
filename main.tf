696ed0b2fe97f792dfa42dc2081e386e6dcd69f3
Guilherme Costa <53190988+gcs-devs@users.noreply.github.com>
2025-02-03 16:45:57 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,61 +1,90 @@
-  container_access_type = "container"
-
+provider "azurerm" {
+  features {}
 }
 
- 
+data "azurerm_client_config" "current" {}
 
-resource "azurerm_key_vault" "kv" {
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "brazilsouth"
+}
 
-  name                = "examplekeyvault"
-
+resource "azurerm_disk_access" "setup" {
+  name                = var.diskAccesses_setup_name
   location            = azurerm_resource_group.example.location
-
   resource_group_name = azurerm_resource_group.example.name
+}
 
-  properties {
+resource "azurerm_data_share_account" "teste" {
+  name                = var.accounts_teste_name
+  location            = "southcentralus"
+  resource_group_name = azurerm_resource_group.example.name
+  identity {
+    type = "SystemAssigned"
+  }
+}
 
-    tenant_id = data.azurerm_client_config.current.tenant_id
+resource "azurerm_storage_account" "procon" {
+  name                     = var.storageAccounts_procon_name
+  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.example.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  kind                     = "StorageV2"
+  enable_https_traffic_only = true
+  allow_blob_public_access = true
+  large_file_share_enabled = true
+  min_tls_version          = "TLS1_2"
 
-    sku {
-
-      name = "standard"
-
+  blob_properties {
+    delete_retention_policy {
+      days = 7
     }
-
   }
 
+  file_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
+
+  queue_properties {}
+  table_properties {}
 }
 
- 
+resource "azurerm_storage_container" "calombo" {
+  name                  = "calombo"
+  storage_account_name  = azurerm_storage_account.procon.name
+  container_access_type = "container"
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                = "examplekeyvault"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  properties {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    sku {
+      name = "standard"
+    }
+  }
+}
 
 resource "azurerm_key_vault_secret" "db_connection" {
-
   name         = "db-connection-string"
-
   value        = "your-db-connection-string"
-
   key_vault_id = azurerm_key_vault.kv.id
-
 }
 
  
 
 resource "azurerm_key_vault_access_policy" "example" {
-
   key_vault_id = azurerm_key_vault.kv.id
-
   tenant_id    = data.azurerm_client_config.current.tenant_id
-
   object_id    = data.azurerm_client_config.current.object_id
 
- 
-
   secret_permissions = [
-
     "Get",
-
     "List",
-
   ]
-
 }
